ff7c6df22ea6f0c3baa0989a961c7bc23825c8f5
EAOE <61086921+EAOE3@users.noreply.github.com>
2021-10-03 21:12:55 +0300

    Update data-team.js

--- src/pages/home/data-team.js
+++ src/pages/home/data-team.js
@@ -12,7 +12,7 @@ const team = [
         imageurl: member1,
         name: 'BlessedSon',
         charge: 'Executive Producer',
-        socialMedia: <a href="https://twitter.com/theblessedson17">'@theblessedson17'</a>
+        socialMedia: '<a href="https://twitter.com/theblessedson17">@theblessedson17</a>'
     }, {
         imageurl: member2,
         name: 'Hashem Zaini',
