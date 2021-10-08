e15b13a61211bb993dafabbe39b5016fb9963966
loque <madridx2010@gmail.com>
2021-10-08 12:33:30 -0500

    stream section added

--- src/pages/stream/index.js
+++ src/pages/stream/index.js
@@ -1,20 +1,49 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const fetchEpisodes = async () => {
-    // const res = await fetch('https://drive.google.com/uc?export=view&id=1_ySSZ1hdgTM1oQMY6beXW7UonSHmJyoh');
-    // console.log(res);
-}
+
 
 const Stream = props => {
 
+    const [episodes, setEpisodes] = useState([]);
+
+    console.log(episodes);
     useEffect(
         () => {
+            const fetchEpisodes = async () => {
+                const res = await axios.get('./episodes/data.json');
+                setEpisodes(res.data.episodes);
+                console.log(res.data.episodes);
+            }
             fetchEpisodes();
         }, []
     );
 
     return(
-        <div>
+        <div className="has-background-black p-6" style={{width: '100%', minHeight: '100%'}}>
+
+            <div className="container" style={{height: '100%'}}>
+
+                <div className="columns is-multiline">
+                    {
+                        episodes.map( (e,i) =>
+                            <div className="column is-3-desktop is-6-tablet" key={i}>
+                                <figure class="image is-16by9">
+                                    <iframe class="has-ratio" width="640" height="360" src={"https://www.youtube.com/embed/"+e.video_id} frameborder="0" allowfullscreen></iframe>
+                                </figure>
+                                <div className="mt-4" style={{height: '200px'}}>
+                                    <h1 className="title has-text-white is-5">{e.name}</h1>
+                                    <p className="has-text-" style={{color: '#b5b5b5'}}>
+                                        {
+                                            e.description
+                                        }
+                                    </p>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
         </div>
     );
 }
